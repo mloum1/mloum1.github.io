a22fbac1c1e5049f1cf27eaa7d0bf118ff2f5e57
Maniang Loum <maniloum4@gmail.com>
2023-01-24 20:57:36 +0100

    mise en ligne portfolio

---
+++ main.js
@@ -0,0 +1,73 @@
+"use strict";
+
+//le string à utiliser
+
+const text = `Aprés une licence en Langues étrangères appliquées, je suis tourné vers l'informatique et je poursuis actuellement un 
+   Master en informatique à l'université de Montpellier. Je suis à la recherche d'une alternance pour le Master 2 en tant que développeur whatsoever.`;
+//compteur initialiser à zéro j'ai utiliser let car elle sera globale et je pourrai l'utiliser à l'intérieur d'une fonction
+let i = 0;
+//variable de la vittesse
+const speed = 10;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(function () {
+      i = 0;
+      document.getElementById("text").innerHTML = "";
+      typeWriter();
+    }, 5000);
+  }
+}
+typeWriter();
+
+//Revealing elements on scrolling  using the
+const allSections = document.querySelectorAll(".section");
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section__hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section__hidden");
+});
+//
+
+// const sky = document.querySelector(".sky");
+// const airplane = document.querySelector(".airplane");
+
+// sky.addEventListener("mouseover", () => {
+//   airplane.classList.add("flying");
+// });
+
+// sky.addEventListener("mouseout", () => {
+//   airplane.classList.remove("flying");
+// // });
+// const airplane = document.querySelector(".airplane");
+// airplane.addEventListener("animationend", () => {
+//   airplane.style.animation = "";
+// });
+
+// li span handle
+
+const spans = document.querySelectorAll(".adventure > li > span ");
+console.log(spans);
+spans.forEach((span) => {
+  span.addEventListener("mouseover", () => {
+    span.style.border = " 7px solid yellow";
+  });
+  span.addEventListener("mouseout", () => {
+    span.style.border = "";
+  });
+});
+
+///testing
