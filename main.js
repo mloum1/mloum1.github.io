d6a88284ad6700a8208b2f298c874d88c59906b5
Maniang Loum <maniloum4@gmail.com>
2023-01-28 16:55:42 +0100

    ajout des pop-up ou des fenetres modal

--- main.js
+++ main.js
@@ -53,3 +53,42 @@ const annee = document.querySelector(".year");
 const date = new Date();
 
 annee.innerHTML = "      " + date.getFullYear() + "   " + date.getDay();
+
+/// les modal
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const sibd = document.getElementById("sibd");
+// toutes les classes
+
+const connect = document.getElementById("connectModal");
+// Modal window
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+  if (!modal.classList.contains("registerModal")) {
+  }
+});
